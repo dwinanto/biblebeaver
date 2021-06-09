1fc3f8748bba88c58ce220b854f05450e925e863
Dwinanto Santoso <dwinanto.santoso@gmail.com>
2021-06-10 00:04:31 +0800

    add vue plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const eleventyVue = require("@11ty/eleventy-plugin-vue");
+
 module.exports = function(eleventyConfig){
+
+   eleventyConfig.addPlugin(eleventyVue);
+
    eleventyConfig.addPassthroughCopy('images')
    eleventyConfig.addPassthroughCopy('admin') 
 }
